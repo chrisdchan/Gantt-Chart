b763bd0de6f99c7d92e8a8a92fabc8ca0d002691
chrisdchan <christopherdchan02@gmail.com>
2022-06-03 01:23:19 -0400

    Updated TaskDisplay class
    added funcationality for tasks being cutoff by display and built constructor

--- Gaant Chart/Models/TaskDisplay.cs
+++ Gaant Chart/Models/TaskDisplay.cs
@@ -10,27 +10,79 @@ namespace Gaant_Chart.Models
 {
     public class TaskDisplay
     {
-        DateTime day { get; set; }
-        SolidColorBrush color { get; }
+        private double pixelsPerDay { get; set; }
+        Task task { get; }
 
-        Rectangle rectangle { get; set; }
+        public Rectangle rectangle { get; set; }
+        public SolidColorBrush color { get; }
+        private double numDays { get; set; }
+
+        private DateTime start { get; set; }
+        private DateTime end { get; set; }
 
         const double DEFAULT_WIDTH = 23;
         const double DEFAULT_HEIGHT = 30;
 
-        public TaskDisplay(DateTime day, SolidColorBrush color)
+        private SolidColorBrush COMPLETED_COLOR = new SolidColorBrush(Color.FromRgb(149, 219, 139));
+        private SolidColorBrush PLANNED_COLOR = new SolidColorBrush(Color.FromRgb(254, 212, 158));
+
+        public TaskDisplay(Task task, double pixelsPerDay)
         {
-            this.day = day;
-            this.color = color;
-
+            this.task = task;
+            this.pixelsPerDay = pixelsPerDay;
             rectangle = new Rectangle();
-            rectangle.Fill = color;
 
-            rectangle.Width = DEFAULT_WIDTH;
-            rectangle.Height = DEFAULT_HEIGHT;
+            if (task.completed)
+            {
+                initCompletedTask();
+            }
+            else
+            {
+                initPlannedTask();
+            }
+
+            numDays = (start - end).TotalDays;
+
+            resizeTask(pixelsPerDay);
+        }
+
+        private void initCompletedTask()
+        {
+            rectangle.Fill = COMPLETED_COLOR;
+            start = task.startDate;
+            end = task.endDate;
+        }
+
+        private void initPlannedTask()
+        {
+            rectangle.Fill = PLANNED_COLOR;
+            start = task.plannedStartDate;
+            end = task.plannedEndDate;
+        }
+
+        public void resizeTask(double pixelsPerDay)
+        {
+            rectangle.Width = pixelsPerDay * numDays;
+        }
+
+        public void cutOffTask(DateTime lastDateDisplayed)
+        {
+            if(isCutOff(lastDateDisplayed))
+            {
+                double numDaysDisplay = (lastDateDisplayed - start).TotalDays;
+                rectangle.Width = pixelsPerDay * numDaysDisplay;
+            }
+
+        }
+
+        private Boolean isCutOff(DateTime lastDateDisplayed)
+        {
+            return (start < lastDateDisplayed && lastDateDisplayed < end);
         }
 
 
 
     }
+
+
 }
