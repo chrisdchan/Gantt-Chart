e6a51bcdd6d1581ef4a81be55136e5c31145cd0c
Chris <christopherdchan02@gmail.com>
2022-07-05 14:16:54 -0400

    Create ExcelReader class

---
+++ Gaant Chart/Models/ExcelReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gaant_Chart.Models
+{
+    public class ExcelReader
+    {
+        public Model extractModelFromExel(String pathname)
+        {
+            Excel.Application xlApp = new Excel.Application();
+        }
+    }
+}
